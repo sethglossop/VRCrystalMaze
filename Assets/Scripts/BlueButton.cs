8f9e6d4d61836e69526d146160082efa8ba2830d
chantheboy <56391666+chantheboy@users.noreply.github.com>
2019-11-22 16:28:38 +0000

    Futuristic Level interactions

---
+++ Assets/Scripts/BlueButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class BlueButton : MonoBehaviour
+{
+    private Interactable interactable;
+    private bool on = false;
+    public LightColour lamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<Interactable>();
+    }
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+        bool isGrabEnding = hand.IsGrabEnding(this.gameObject);
+
+        if (interactable.attachedToHand == null && startingGrabType != GrabTypes.None)
+        {
+            lamp.Blue();
+            OnOff();
+        }
+
+    }
+    private void OnOff()
+    {
+        on = !on;
+        if (on)
+        {
+            transform.position = transform.position + new Vector3(0, -0.03f, 0);
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(0, 0.03f, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/LightColour.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightColour : MonoBehaviour
+{
+    private Light lamp;
+    private bool red = false;
+    private bool blue = false;
+    private bool green = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lamp = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (red && blue && green)
+        {
+            lamp.color = Color.white;
+        }
+        else if (red && blue && !green)
+        {
+            lamp.color = Color.magenta;
+        }
+        else if (red && !blue && green)
+        {
+            lamp.color = Color.yellow;
+        }
+        else if (!red && blue && green)
+        {
+            lamp.color = Color.cyan;
+        }
+        else if (red && !blue && !green)
+        {
+            lamp.color = Color.red;
+        }
+        else if (!red && blue && !green)
+        {
+            lamp.color = Color.blue;
+        }
+        else if (!red && !blue && green)
+        {
+            lamp.color = Color.green;
+        }
+        else
+        {
+            lamp.color = Color.black;
+        }
+
+    }
+
+    public void Red()
+    {
+        red = !red;
+    }
+    public void Blue()
+    {
+        blue = !blue;
+    }
+    public void Green()
+    {
+        green = !green;
+    }
+}
