693377a4cc4e1a8e726309d0880841beb98b5e6c
chantheboy <56391666+chantheboy@users.noreply.github.com>
2019-11-29 17:02:21 +0000

    Dome Movement

---
+++ Assets/Scripts/Medieval/TicketMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketMove : MonoBehaviour
+{
+    public GameObject ticket;
+    private GameObject newTicket;
+    private float maxradius = 5;
+    private float maxhorizspeed = 5;
+    private float maxvertspeed = 3;
+    private float radius;
+    private float horizspeed;
+    private float vertspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        horizspeed = Random.Range(1, maxhorizspeed);
+        vertspeed = Random.Range(1, maxvertspeed);
+        radius = Random.Range(1, maxradius);
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector3 circle = new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle));
+        newTicket = Instantiate(ticket, transform.position + circle, Random.rotation);
+        newTicket.transform.parent = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, horizspeed, 0);
+        transform.Translate(0, vertspeed*Time.deltaTime, 0);
+        if (transform.position.y < 0 || transform.position.y > 4)
+        {
+            vertspeed = -vertspeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Medieval/TicketSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TicketSpawn : MonoBehaviour
+{
+    public int goldquantity;
+    public int silverquantity;
+    public GameObject goldspawn;
+    public GameObject silverspawn;
+    public float maxHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < goldquantity; i++ )
+        {
+            Vector3 height = new Vector3(0,Random.Range(0, maxHeight),0);
+            Instantiate(goldspawn, transform.position + height,Quaternion.identity);
+        }
+        for (int i = 0; i < silverquantity; i++)
+        {
+            Vector3 height = new Vector3(0, Random.Range(0, maxHeight), 0);
+            Instantiate(silverspawn, transform.position + height, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
